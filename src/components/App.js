4d9b31a94d0ec5712fc0fde4e89ab0fb8c967c1c
lee <hoangleenhan@gmail.com>
2019-01-21 15:09:35 +0700

    Fix header router

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import './App.css';
 
-import { CartProvider } from '../contexts/Cart';
+import {CartProvider} from '../contexts/Cart';
 import Header from './Header';
 import Product from '../pages/Product';
 import Home from '../pages/Home';
@@ -12,24 +12,24 @@ import About from '../pages/About';
 import Cart from '../pages/Cart';
 
 class App extends Component {
-  render() {
-    return (
-      <CartProvider>
-        <div className='App'>
-          <Header />
-          <Router>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/products/:id' component={Product} />
-              <Route path='/products' component={ProductList} />
-              <Route path='/about' component={About} />
-              <Route path='/cart' component={Cart} />
-            </Switch>
-          </Router>
-        </div>
-      </CartProvider>
-    )
-  }
+    render() {
+        return (
+            <Router>
+                <CartProvider>
+                    <div className='App'>
+                        <Header/>
+                        <Switch>
+                            <Route exact path='/' component={Home}/>
+                            <Route path='/products/:id' component={Product}/>
+                            <Route path='/products' component={ProductList}/>
+                            <Route path='/about' component={About}/>
+                            <Route path='/cart' component={Cart}/>
+                        </Switch>
+                    </div>
+                </CartProvider>
+            </Router>
+        )
+    }
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,45 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
 
-import { CartContext } from '../contexts/Cart';
+import {CartContext} from '../contexts/Cart';
 
 class Header extends React.Component {
-	render() {
-		return (
-			<Router>
-				<header>
-					<nav className='navbar fixed-top navbar-expand-lg navbar-light bg-light'>
-						<a className='navbar-brand' href='/'>Shopping</a>
-						<div className='collapse navbar-collapse'>
-							<ul className='navbar-nav mr-auto'>
-								<li className='nav-item active' style={{ paddingRight: '15px' }}>
-									<Link to='/'>Home</Link>
-									{/* <a href='/'>Home</a> */}
-								</li>
-								<li className='nav-item' style={{ paddingRight: '15px' }}>
-									<Link to='/products'>Products</Link>
-									{/* <a href='products'>Products</a> */}
-								</li>
-								<li className='nav-item' style={{ paddingRight: '15px' }}>
-									<Link to='/about'>About</Link>
-									{/* <a href='/about'>About</a> */}
-								</li>
-							</ul>
-							<form className='form-inline'>
-								<input className='form-control mr-sm-2' type='search' placeholder='search' />
-								<button className='btn btn-outline-success my-2 my-sm-0' type='Submit'>Search</button>
-							</form>
-						</div>
-						<li className='nav-item' style={{ padding: '0 15px', listStyle: 'none' }}>
-							<CartContext.Consumer>
-								{({ cartItems }) => (
-									<Link to='/cart'>Cart ({cartItems.length})</Link>
-								)}
-							</CartContext.Consumer>
-						</li>
-					</nav>
-				</header>
-			</Router>
-		);
-	}
+    render() {
+        return (
+            <header>
+                <nav className='navbar fixed-top navbar-expand-lg navbar-light bg-light'>
+                    <a className='navbar-brand' href='/'>Shopping</a>
+                    <div className='collapse navbar-collapse'>
+                        <ul className='navbar-nav mr-auto'>
+                            <li className='nav-item active' style={{paddingRight: '15px'}}>
+                                <Link to='/'>Home</Link>
+                                {/* <a href='/'>Home</a> */}
+                            </li>
+                            <li className='nav-item' style={{paddingRight: '15px'}}>
+                                <Link to='/products'>Products</Link>
+                                {/* <a href='products'>Products</a> */}
+                            </li>
+                            <li className='nav-item' style={{paddingRight: '15px'}}>
+                                <Link to='/about'>About</Link>
+                                {/* <a href='/about'>About</a> */}
+                            </li>
+                        </ul>
+                        <form className='form-inline'>
+                            <input className='form-control mr-sm-2' type='search' placeholder='search'/>
+                            <button className='btn btn-outline-success my-2 my-sm-0' type='Submit'>Search</button>
+                        </form>
+                    </div>
+                    <li className='nav-item' style={{padding: '0 15px', listStyle: 'none'}}>
+                        <CartContext.Consumer>
+                            {({cartItems}) => (
+                                <Link to='/cart'>Cart ({cartItems.length})</Link>
+                            )}
+                        </CartContext.Consumer>
+                    </li>
+                </nav>
+            </header>
+        );
+    }
 }
 
 export default Header;
